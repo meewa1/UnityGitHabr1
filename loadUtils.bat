c2a5387bf7ebb3013bd00030d1c2d45a893eb277
Meewa <mamatyukov@yandex.ru>
2020-06-29 14:11:01 +0700

    mod batch

--- loadUtils.bat
+++ loadUtils.bat
@@ -1,6 +1,8 @@
 SET SCRIPTPATH=Assets\Scripts
 
-curl -o "%SCRIPTPATH%\Utils.cs" -L https://raw.githubusercontent.com/meewa1/Utils/master/Utils.cs
+SET UTILSFILE=Utils.cs
+
+curl -o "%SCRIPTPATH%\%UTILSFILE%" -L https://raw.githubusercontent.com/meewa1/Utils/master/%UTILSFILE%
 
 git add .
 git commit --amend --no-edit
