6c130c54b9eba319e94736e18eb3fc89ebbf8c33
Meewa <mamatyukov@yandex.ru>
2020-06-29 14:06:08 +0700

    batch script

--- Assets/Scripts/Utils.cs
+++ Assets/Scripts/Utils.cs
@@ -1,7 +1,14 @@
+// all files
+
 namespace Ruina
 {
     public class Utils
     {
         //TODO: somehow include functions from ./Functions/ and load their libraries
     }
+}
+//using %library%
+public void FunctionName()
+{
+    return;
 }
